ad5b17d31763b65d9a547ca54fdccda22e228267
m-mimayoto <85854132+m-mimayoto@users.noreply.github.com>
2021-07-18 11:44:21 +0900

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="l-footer">
-            <div class="p-footer__contents">
-                <p class="p-footer__contents__items"><a class="p-footer__contents__item">ショップ情報</a>｜<a class="p-footer__contents__item">ヒストリー</a></p>
-                <p class="p-footer__contents__note">Copyright: RaiseTech</p>
-            </div>
-</footer>
-</body>
-<?php wp_footer(); ?>
-</html>
